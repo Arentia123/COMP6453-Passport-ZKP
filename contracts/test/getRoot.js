cb3512f3d4999fd578a315b3ffbd6fba77a6f5c9
“xinran” <z5438685@ad.unsw.edu.au>
2024-07-16 21:55:51 +1000

    update merkle tree

--- contracts/test/getRoot.js
+++ contracts/test/getRoot.js
@@ -4,20 +4,19 @@ const { Web3 } = require('web3');
 const web3 = new Web3('http://localhost:7545');
 
 // // Your account private key and the contract address
-const privateKey = '0x56b0ecb8deca267e1f083ce88793108691d905328b99908245fe66ae2e5f4d4f';
-const contractAddress = '0x6fbdd05d438aD578D5BC70fcf881ddDf6c1C6840';
+const privateKey = '0x80eb63ef37a8d3a14835cc859a95d2c820b3568c0ece51601c526292859198bb';
+const contractAddress = '0x26E7075b82315d6ACefD84620aF2A2b388777429';
 
 const contract = new web3.eth.Contract(certificate, contractAddress);
 
-const removeCertificate = async () => {
+const getRoot = async () => {
     const account = web3.eth.accounts.privateKeyToAccount(privateKey);
     web3.eth.accounts.wallet.add(account);
     web3.eth.defaultAccount = account.address;
-    console.log(account);
-    const _hash = '0x1';
-    const receipt = await contract.methods.removeCertificate(_hash).send({ from: account.address});
+
+    const receipt = await contract.methods.getRoot().call({ from: account.address});
 
     console.log(receipt);
 }
 
-removeCertificate();
+getRoot();
